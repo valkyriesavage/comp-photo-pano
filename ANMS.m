519c6559ff234e088a84ecd39c1af0f684c47538
Valkyrie Savage <valkyrie.savage@gmail.com>
2014-11-11 14:13:42 -0800

    alyosha harris code and anms code

---
+++ ANMS.m
@@ -0,0 +1,31 @@
+function points = ANMS(x,y,v,numpts)
+    % Adaptive Non-Maximal Suppression
+    
+    pts = [x,y];
+    
+    % ok, we start by finding a list of radii for which each point will be
+    % included in our final set of points.
+    rads = [];
+    c_robust = 0.9;
+    cd_v = c_robust*v;
+    D = pdist(pts,'euclidean');
+    D = squareform(D);
+    for i = [1:size(pts,1)]
+        dists_i = D(i,:);
+        strong_enoughs = ones(size(cd_v))*999;
+        strong_enoughs((cd_v - v(i)) > 0) = dists_i((cd_v - v(i)) > 0);
+        [M,~] = min(strong_enoughs);
+        rads(i) = M;
+    end
+    
+    % then we find the radius that gives us numpts points
+    for rad = sort(rads,'descend')
+        if(size(rads(rads>rad),2) >= numpts)
+            break;
+        end
+    end
+    
+    % now we need to make a collection of all points who are the strongest
+    % in that particular radius
+    points = pts(rads>rad,:);
+end
--- main.m
+++ main.m
@@ -1,6 +1,9 @@
-HAWAII = false;
-ROSE_GARDEN = true;
+HAWAII = true;
+ROSE_GARDEN = false;
 MUNICH = false;
+
+USER = false;
+AUTO = true;
 
 % load images
 
@@ -26,77 +29,76 @@ if MUNICH
 
 end
 
-% load user-selected homography point pairs
+% load homography point pairs
 
-if HAWAII
-    load 'cps-hawaii.mat'
+if USER
+    if HAWAII
+        load 'cps-hawaii.mat'
+    end
+    if ROSE_GARDEN
+        load 'cps-rose.mat'
+    end
+    if MUNICH
+        load 'cps-munich.mat'
+    end
+    
+    % now refine automatically
+
+    ohto1 = refine(ohto1,im0,im1);
+    oneto2 = refine(oneto2,im1,im2);
 end
-if ROSE_GARDEN
-    load 'cps-rose.mat'
-end
-if MUNICH
-    load 'cps-munich.mat'
+if AUTO
+    [ohx,ohy,ohv] = harris(im0);
+    [onex,oney,onev] = harris(im1);
+    [twox,twoy,twov] = harris(im2);
+    
+    numpts = 500;
+    ohpts = ANMS(ohx,ohy,ohv,numpts);
+    onepts = ANMS(onex,oney,onev,numpts);
+    twopts = ANMS(twox,twoy,twov,numpts);
+    
+    ohto1 = struct;
+    oneto2 = struct;
+    ohto1.inputPoints = ohpts;
+    ohto1.basePoints = onepts;
+    oneto2.basePoints = twopts;
+    oneto2.inputPoints = onepts;
+    
+    ohto1 = russianGranny(ohto1);
+    oneto2 = russianGranny(oneto2);
+    
+    ohto1 = RANSAC(ohto1);
+    oneto2 = RANSAC(oneto2);
 end
 
-% now refine automatically
-
-ohto1 = refine(ohto1,im0,im1);
-oneto2 = refine(oneto2,im1,im2);
-% twoto3 = refine(twoto3,im2,im3);
 
 % a note about these points: for each structure it's like this:
 % ohto1.basePoints = points in im1
 % ohto1.inputPoints = points in im0
 
 % now that we have refined points, make some dang homographies!
+% these homographies should take
+% image 0 to image 1, image 2 to image 1
 
 h01 = computeH(ohto1.inputPoints,ohto1.basePoints);
-% h12 = computeH(oneto2.inputPoints,oneto2.basePoints);
 h21 = computeH(oneto2.basePoints,oneto2.inputPoints);
-% h23 = computeH(twoto3.inputPoints,twoto3.basePoints);
-
-% so, just to be clear, these homographies should take
-% image 0 to image 1, image 1 to image 2, image 2 to image 3
 
 % before we warp, let's set some alphas.
 im0 = alphanate(im0);
 im1 = alphanate(im1);
 im2 = alphanate(im2);
-% im3 = alphanate(im3);
 
 % now, to warp! -- let's restrict to 3 images and 
 % just warp the outer two into the center.
 [imwarp0in1,xshift01,yshift01] = warpImage(im0,h01);
-% [imwarp0in2,xshift02,yshift02] = warpImage(imwarp0in1,h12);
-% [imwarp0in3,xshift03,yshift03] = warpImage(imwarp0in2,h23);
-% xshift0 = sum([xshift01,xshift02,xshift03]);
-% yshift0 = sum([yshift01,yshift02,yshift03]);
-
-% [imwarp1in2,xshift12,yshift12] = warpImage(im1,h12);
-% [imwarp1in3,xshift13,yshift13] = warpImage(imwarp1in2,h23);
-% xshift1 = sum([xshift12,xshift13]);
-% yshift1 = sum([yshift12,yshift13]);
-
 [imwarp2in1,xshift21,yshift21] = warpImage(im2,h21);
 
-% [imwarp2in3,xshift23,yshift23] = warpImage(im2,h23);
-% xshift2 = sum([xshift23]);
-% yshift2 = sum([yshift23]);
-
 % now we shift
-% im0s = imwarp0in3;
-% im1s = shiftImage(imwarp1in3,xshift0-xshift1,yshift0-yshift1);
-% im2s = shiftImage(imwarp2in3,xshift0-xshift2,yshift0-yshift2);
-% im3s = shiftImage(im3,xshift0,yshift0);
 
 im0z = shiftImage(imwarp0in1,0,0);
 im1z = shiftImage(im1,xshift01,yshift01);
 im2z = shiftImage(imwarp2in1,xshift01-xshift21,yshift01-yshift21);
 stitchedz = stitchImages(im0z,im1z,im2z);
 
-imshow(stitchedz(:,:,1:3));
-
 % aaaand we stitch
-% stitched = stitchImages(im0s,im1s,im2s,im3s);
-% 
-% H=imshow(stitched);
+imshow(stitchedz(:,:,1:3));
